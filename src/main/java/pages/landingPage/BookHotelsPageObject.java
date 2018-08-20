558a55b74897f260616d764f173666c54b418a9c
Māris Lagzdiņš (TestDevLab) <maris.lagzdins@testdevlab.com>
2018-08-20 18:01:54 +0300

    Created new feature file Added elements to the BookHotelPageObject class.

--- src/main/java/pages/landingPage/BookHotelsPageObject.java
+++ src/main/java/pages/landingPage/BookHotelsPageObject.java
@@ -1,5 +1,53 @@
 package pages.landingPage;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class BookHotelsPageObject {
 
+    private SelenideElement getBookHotelsButton() {
+        return $("li[data-title='HOTELS'] > a");
+    }
+
+    private SelenideElement getHotelTextField() {
+        return $("#s2id_autogen9");
+    }
+
+    private SelenideElement getHotelElementFromList(String hotel) {
+        return $(By.xpath("//ul[@class='select2-result-sub']//li[//*[contains(text(), '" + hotel + "')]]"));
+    }
+
+    private SelenideElement getCheckInTextField() {
+        return $("input[name='checkin']");
+    }
+
+    private SelenideElement getCheckOutTextField() {
+        return $("input[name='checkout']");
+    }
+
+    public void selectBookHotelsButton() {
+        getBookHotelsButton().click();
+    }
+
+    public void selectHotelField() {
+        getHotelTextField().click();
+    }
+
+    public void enterHotelName(String name) {
+        getHotelTextField().sendKeys(name);
+    }
+
+    public void selectHotelFromList(String hotel) {
+        getHotelElementFromList(hotel).click();
+    }
+
+    public void enterCheckInDate(String day, String month, String year) {
+        getCheckInTextField().sendKeys(day + "/" + month + "/" + year);
+    }
+
+    public void enterCheckOutDate(String day, String month, String year) {
+        getCheckOutTextField().sendKeys(day + "/" + month + "/" + year);
+    }
 }
--- src/test/java/stepdefinitions/HomepageSteps.java
+++ src/test/java/stepdefinitions/HomepageSteps.java
@@ -1,5 +1,6 @@
 package stepdefinitions;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.*;
 import general.TestContext;
 
@@ -31,11 +32,42 @@ public class HomepageSteps {
         test.getNavigation().selectLoginButton();
     }
 
-    @And("^I am in Sign up form$")
+    @And("^I am in Signup form$")
     public void iAmInSignUpForm() {
         iHaveOpenedHomePage();
         iSelectMyAccountMenu();
         iSelectSignUpButton();
     }
 
+
+    @And("^I select book Hotels button$")
+    public void iSelectBookHotelsButton() {
+        test.getBookHotelsPage().selectBookHotelsButton();
+    }
+
+    @And("^I enter ([^\\”]*) hotel$")
+    public void iEnterHotelName(String hotel) {
+        test.getBookHotelsPage().selectHotelField();
+        test.getBookHotelsPage().enterHotelName(hotel);
+    }
+
+    @And("^I select hotel located in ([^\\”]*)")
+    public void iSelectHotelWithName(String hotelName) {
+        test.getBookHotelsPage().selectHotelFromList(hotelName);
+    }
+
+    @And("^I select (\\w+).(\\w+).(\\w+) as Check in date$")
+    public void iSelectAsCheckInDate(String day, String month, String year) {
+        test.getBookHotelsPage().enterCheckInDate(day, month, year);
+    }
+
+    @And("^I select (\\w+).(\\w+).(\\w+) as Check out date$")
+    public void iSelectAsCheckOutDate(String day, String month, String year) {
+        test.getBookHotelsPage().enterCheckOutDate(day, month, year);
+    }
+
+    @And("^I select (\\d+) Adult and (\\d+) Children$")
+    public void iSelectAdultAndChildren(int adultCount, int childrenCount) {
+        // TODO: ...
+    }
 }
